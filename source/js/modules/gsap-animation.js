94f6269524ffd663ce24b6d5bbe86991be1ed777
NadyaChernykh <nchernykh1@gmail.com>
2023-03-01 17:21:10 +0300

    Добавляет блок с меню

--- source/js/modules/gsap-animation.js
+++ source/js/modules/gsap-animation.js
@@ -3,18 +3,13 @@ import {ScrollTrigger} from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
-// Intro animation
-let tlMain = gsap.timeline();
-const main = document.querySelector('main');
-
-gsap.from('.main-nav__item', {opacity: 0, y: -100, duration: 1, stagger: 0.2});
-gsap.from('.socials__item', {opacity: 0, y: 100, duration: 1, stagger: 0.2}, 1);
+let mm = gsap.matchMedia();
+const breakPoint = '(min-width: 768px)';
+const subtitle = document.querySelector('.intro__text');
+const title = document.querySelector('.intro__title');
+const introBlock = document.querySelector('.intro');
 
 const titleAnim = () => {
-  const title = document.querySelector('.intro__title');
-  const subtitle = document.querySelector('.intro__text');
-
-  gsap.to(subtitle, {x: 300, y: -50, scale: 1.5, duration: 2});
 
   const splitText = (el) => {
     el.innerHTML = el.textContent.replace(/(\S*)/g, (m) => {
@@ -29,6 +24,7 @@ const titleAnim = () => {
   function random(min, max) {
     return (Math.random() * (max - min)) + min;
   }
+  gsap.to(subtitle, {x: 300, y: -50, scale: 1.5, duration: 2});
 
   Array.from(split.querySelectorAll('.letter')).forEach((el, idx) => {
     gsap.from(el, {
@@ -44,56 +40,78 @@ const titleAnim = () => {
   });
 };
 
-titleAnim();
+mm.add(breakPoint, () => {
+  // Intro animation
 
-// bg animation
+  titleAnim();
+  gsap.from('.main-nav__item', {opacity: 0, y: -100, duration: 1, stagger: 0.2});
+  gsap.from('.socials__item', {opacity: 0, y: 100, duration: 1, stagger: 0.2}, 1);
+  let tl = gsap.timeline();
 
-gsap.to('.intro .container', {
-  scrollTrigger: {
-    trigger: '.intro',
-    markers: true,
-    start: 'top top',
-    scrub: true,
+  // bg animation
+  tl.to('.intro .container', {
+    scrollTrigger: {
+      trigger: '.intro',
+      start: 'top top',
+      scrub: true,
+    },
+    yPercent: 50,
+    scale: 0.8,
+    opacity: 0,
+  })
+      .to('.bg-block__img', {
+        scrollTrigger: {
+          trigger: introBlock,
+          start: 'top top',
+          scrub: true,
+        },
+        scale: 1.5,
+      })
+      .from('.events__row', {
+        scrollTrigger: {
+          trigger: introBlock,
+          start: 'top top',
+          scrub: true,
+        },
+        x: -100,
+        y: -100,
+        opacity: 0,
+        duration: 0.2,
+        stagger: 0.2,
+      })
+      .from('.events__decor-img', {
+        scrollTrigger: {
+          trigger: introBlock,
+          start: 'top top',
+          scrub: true,
+        },
+        x: +100,
+        y: -100,
+        opacity: 0,
+        duration: 0.3,
+        stagger: 0.2,
+      });
 
-  },
-  yPercent: 80,
-  scale: 0.8,
-  xPercent: -80,
-  opacity: 0,
+  // menu animation
+
+  tl.from('.menu__row.right', {
+    scrollTrigger: {
+      trigger: '.menu',
+      start: 'top top',
+      scrub: true,
+    },
+    x: -100,
+    duration: 0.3,
+  })
+      .from('.menu__row.reverse', {
+        scrollTrigger: {
+          trigger: '.menu',
+          start: 'top top',
+          scrub: true,
+        },
+        x: +100,
+        duration: 0.3,
+      });
 });
 
-gsap.to('.bg-block__img', {
-  scrollTrigger: {
-    trigger: '.intro',
-    start: 'top top',
-    scrub: true,
-  },
-  scale: 1.5,
-});
 
-// all page animation
-tlMain.fromTo('.events', {x: '-100%', y: '+100%'}, {y: 0});
-tlMain.from('.events__row', {
-  x: -200,
-  y: -200,
-  opacity: 0,
-  duration: 0.5,
-  stagger: 0.2,
-});
-tlMain.from('.events__decor-img', {
-  x: +200,
-  y: -200,
-  opacity: 0,
-  duration: 0.3,
-  stagger: 0.2,
-});
-tlMain.fromTo('.reviews', {x: '-100%'}, {x: '-200%'});
-
-ScrollTrigger.create({
-  animation: tlMain,
-  trigger: '.wrapper',
-  start: 'top top',
-  end: () => main.offsetWidth / 2,
-  scrub: true,
-  pin: true,
-});
